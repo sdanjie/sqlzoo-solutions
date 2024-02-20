d94391d80325019e0ad1b0dd6342f478c7470615
Danjie <46720237+sdanjie@users.noreply.github.com>
2024-02-20 15:02:24 -0500

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -10,7 +10,13 @@ FROM world
 WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
 --1.3 Show the country and the area for countries with an area between 200,000 and 250,000.
--- BETWEEN allows range checking (range specified is inclusive of boundary values)
 SELECT name, area 
 FROM world
 WHERE area BETWEEN 200000 AND 250000;
+
+--2. SELECT names uses the LIKE operator to check names 
+--2.1 Find the country that start with Y
+SELECT name
+FROM world
+WHERE name LIKE 'Y%';
+
